cd5a4a28aee70ad9eb2cba86d555eaa90397b2f4
leofontolan <leofontolan@gmail.com>
2022-04-29 13:34:16 -0300

    Exemplos Strings

---
+++ chap3/3.3-Strings.ts
@@ -0,0 +1,4 @@
+let string = 'Hello World';
+
+ console.log(string.length);
+ console.log(string[2]);
---
+++ chap3/3.3.1-TemplateStrings.ts
@@ -0,0 +1,12 @@
+let string1 = "Vamos escrever uma string no código fonte \
+em múltiplas linhas, mas no console.log apenas uma.";
+
+let string2 = `Vamos escrever uma string no código fonte
+em múltiplas linhas.`
+
+let string3 = 'Problemas com apóstrofes dentro de aspas simples  O\'Reilly\'s ';
+
+console.log(string1);
+console.log('\n')
+console.log(string2);
+console.log(string3);
