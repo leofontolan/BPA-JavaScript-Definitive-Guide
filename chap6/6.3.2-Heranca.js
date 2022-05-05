7e3a69c179ebd3cbdf4dd6f270776e9ecd3a7412
leofontolan <leofontolan@gmail.com>
2022-05-05 13:43:23 -0300

    Heranca

---
+++ chap6/6.3.2-Heranca.js
@@ -0,0 +1,24 @@
+let o = {}; // o herda métodos de objeto de Object.prototype
+console.log(o.__proto__ === Object.prototype);
+
+o.x = 1; // e agora tem uma propriedade própria x.
+
+
+let p = Object.create(o); //p herda propriedades de o e Object.prototype
+console.log(p.__proto__ === o);
+
+
+p.y = 2; // e tem uma propriedade própria y.
+
+
+let q = Object.create(p); // q herda propriedades de p, o e...
+console.log(q.__proto__ === p);
+
+
+q.z = 3; // ...Object.prototype e possui uma propriedade própria z.
+
+let f = q.toString(); // toString é herdado de Object.prototype
+q.x + q.y // =>3; x e y são herdados de o e p
+console.log(q.x);
+console.log(q.y);
+console.log(q.z);
