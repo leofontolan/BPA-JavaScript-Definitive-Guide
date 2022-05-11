d0dff0733ce9bcf030f88593ebf30fae3e7ef299
leofontolan <leofontolan@gmail.com>
2022-05-11 13:32:56 -0300

    Testando conversao

--- chap3/testeConversao.js
+++ chap3/testeConversao.js
@@ -1,3 +1,12 @@
 let x = 123;
 let y =  x + "";
 console.log(typeof(y));
+
+console.log('\n');
+console.log(typeof(NaN));
+
+
+let num = 123434.6265
+let num1 = num.toFixed();
+console.log(num1);
+console.log(typeof(num1));
