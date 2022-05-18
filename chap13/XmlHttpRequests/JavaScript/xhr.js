ab17b9cd05768334c19ab37d3423dc6a9540c338
leofontolan <leofontolan@gmail.com>
2022-05-18 15:20:06 -0300

    Live Server XmlHttpRequest

--- chap13/XmlHttpRequests/JavaScript/xhr.js
+++ chap13/XmlHttpRequests/JavaScript/xhr.js
@@ -1,2 +1,24 @@
 var xhr = new XMLHttpRequest();
-console.log(xhr);
+var doc;
+
+
+//monitorar o estado do objeto
+//readyState 4 - request completada e response completada
+//status 200 - request bem sucedida
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log(xhr);
+        console.log(xhr.responseText);
+
+        //converter o texto em JSON
+        doc = xhr.response;
+        console.log(JSON.parse(doc));
+    }
+};
+
+//abrir a conexão com método GET
+// Passar a URL do JsonPlaceHolder e se a requisição é assíncrona
+xhr.open("GET", "http://jsonplaceholder.typicode.com/posts/1", true);
+
+//enviar a requisição
+xhr.send();
