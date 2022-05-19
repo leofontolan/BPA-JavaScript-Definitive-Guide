7129d27689f2f4879df495fe295ddf5cfd2f9678
leofontolan <leofontolan@gmail.com>
2022-05-19 16:11:08 -0300

    Callback2

--- chap13/Promises/callback.js
+++ chap13/Promises/callback.js
@@ -3,10 +3,58 @@ const fs = require('fs');
 console.log(1);
 
 
-fs.readFile('chap13/Promises/in1.txt', (err, contents) => {
-    console.log(err, String(contents));
+//=========================================================== CALLBACKS
+// fs.readFile('chap13/Promises/in1.txt', (err, contents) => {
+    
+//     fs.readFile('chap13/Promises/in2.txt', (err, contents2) => {
+//         console.log(contents.toString() + contents2.toString());
+//     }
+//     );}
+// );
+
+
+//=========================================================== PROMISES
+// const rf =  file => new Promise(( resolve, reject ) => {
+
+//     fs.readFile(file, (err, contents) => {
+//         if(err) reject(err);
+//         else resolve(contents);
+//     });
+// } );
+
+// rf('chap13/Promises/in1.txt').then(contents => {
+//     console.log(contents.toString());
+// });
+
+// rf('chap13/Promises/in2.txt').then(contents2 => {  
+//     console.log(contents2.toString());
+//  });
+
+
+const rf =  file => new Promise(( resolve, reject ) => {
+
+    fs.readFile(file, (err, contents) => {
+        if(err) reject(err);
+        else resolve(contents);
+    });
 } );
 
+const init = async() => {
+
+    try{
+        const in1 = await rf('chap13/Promises/in1.txt');
+        const in2 = await rf('chap13/Promises/in2.txt');
+        console.log(in1.toString() + in2.toString());
+        return in1.toString() + in2.toString();
+
+    }catch(err){
+        console.log(err);
+    }
+};
+
+init().then( contents => console.log(contents));
+
+console.log(init());
 
 console.log(2);
 
