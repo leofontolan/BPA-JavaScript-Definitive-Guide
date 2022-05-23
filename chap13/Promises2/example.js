063c35acefd821ed68c1228c665fbb9cfabdeae6
leofontolan <leofontolan@gmail.com>
2022-05-23 11:34:04 -0300

    Lendo o arquivo de forma assíncrona

---
+++ chap13/Promises2/example.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+
+console.log(1);
+
+const fileContentSync = fs.readFileSync('./chap13/Promises2/test.txt', 'utf8');
+console.log(fileContentSync);
+
+
+const fileContent = fs.readFile('./chap13/Promises2/test.txt', (err, content) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(content);
+    }
+});
+
+console.log(2);
+
+console.log(3);
