0aff4a42c363b68b55c3d217517f21fa3654fbc5
leofontolan <leofontolan@gmail.com>
2022-05-26 11:31:07 -0300

    Teste promise pending

--- chap13/Promises2/test/new_promise.js
+++ chap13/Promises2/test/new_promise.js
@@ -1,5 +1,15 @@
 class NPromise{
     
+    constructor(executor){
+        if(typeof executor !== 'function'){
+            throw new Error('Promise executor must be a function');
+        }
+
+        this.state = 'pending';
+        this.value = undefined;
+        this.onFulfillChain = [];
+        this.onRejectChain = [];
+    }
 }
 
 module.exports = NPromise;
--- chap13/Promises2/test/promise.test.js
+++ chap13/Promises2/test/promise.test.js
@@ -3,5 +3,8 @@ const NPromise = require('./new_promise');
 describe ('Promise', () => {
   it('should create a new Promise with pending state', () => {
 
+    const promise = new NPromise(() => {});
+    expect(promise.state).toBe('pending');
+    expect(promise.value).toBe(undefined);
   });
 });
