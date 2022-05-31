1bcd824a6be2c1f182d39b6216b39a1276162c60
leofontolan <leofontolan@gmail.com>
2022-05-31 10:06:36 -0300

    Exemplo codigo assíncrono

---
+++ chap13/Promises2/callbacks/exemple.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+console.log(1);
+
+// Exemplo de função síncrona. Lendo um arquivo do disco.
+// A execução do código é bloqueada até o arquivo ser lido.
+const fileContentSync = fs.readFileSync('chap13/Promises2/callbacks/test.txt', 'utf8');
+console.log(fileContentSync);
+
+
+console.log(2);
+
+// Exemplo de função assíncrona. Lendo um arquivo do disco.
+// A execução do código não é bloqueada até o arquivo ser lido.
+const fileContent = fs.readFile('chap13/Promises2/callbacks/test.txt', 'utf8', 
+        (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data);
+            }
+        });
+
+console.log(3);
+
+
+console.log(4);
+
